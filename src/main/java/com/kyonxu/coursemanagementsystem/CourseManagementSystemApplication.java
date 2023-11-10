f5a2ac07924e47b454a4973e20f31f8f645c8c21
Kyon <1260328617@qq.com>
2023-11-10 22:17:46 +0800

    Init project

---
+++ src/main/java/com/kyonxu/coursemanagementsystem/CourseManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.kyonxu.coursemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSystemApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementSystemApplication.class, args);
+	}
+
+}
